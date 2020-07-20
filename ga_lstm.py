7d80ddd4fdb2919860a987c8825d9ef58ced6fb5
cuifeng <cuifeng_fine@163.com>
2020-07-20 19:50:41 +0800

    读取csv

--- ga_lstm.py
+++ ga_lstm.py
@@ -1,15 +1,251 @@
 import tensorflow as tf
+import np as np
+
+import os
 
 from data_process import csv_to_dataset
 
 
-def main():
+tf.keras.backend.set_floatx('float32')
 
-    X, Y = csv_to_dataset(r'Metro_Interstate_Traffic_Volume.csv')
+class Attention(tf.keras.layers.Layer):
+  def __init__(self, units_num):
+    super(Attention, self).__init__()
+    self.W1 = tf.keras.layers.Dense(units_num)
+    self.W2 = tf.keras.layers.Dense(units_num)
+    self.W3 = tf.keras.layers.Dense(units_num)
+    self.V = tf.keras.layers.Dense(1)
 
-    pass
+  def call(self, query, values):
+
+    hidden_with_time_axis_h = tf.expand_dims(query[0], 1)
+    hidden_with_time_axis_c = tf.expand_dims(query[1], 1)
+
+    score = self.V(tf.nn.tanh(self.W1(values) + self.W2(hidden_with_time_axis_h)+self.W3(hidden_with_time_axis_c)))
+
+    attention_weights = tf.nn.softmax(score, axis=1)
+
+    context_vector = attention_weights * values
+    context_vector = tf.reduce_sum(context_vector, axis=1)
+
+    return context_vector, attention_weights
+
+class Encoder(tf.keras.Model):
+    def __init__(self, enc_units, batch_sz):
+        super(Encoder, self).__init__()
+        self.batch_sz = batch_sz
+        self.enc_units = enc_units
+
+        #可以考虑通过ga调整的参数  recurrent_initializer 有多个值（orthogonal、glorot_uniform）
+        self.lstm = tf.keras.layers.LSTM(self.enc_units, return_sequences=True, return_state=True, recurrent_initializer='glorot_uniform')
+
+    def call(self, x, hidden):
+
+        self.lstm.states[0] = hidden[0]
+        self.lstm.states[1] = hidden[1]
+
+        output, state_h, state_c = self.lstm(x)
+
+        return output, state_h, state_c
+
+    def initialize_hidden_state(self):
+        return tf.zeros([self.batch_sz, self.enc_units])
+
+class Decoder(tf.keras.Model):
+  def __init__(self, result_size, dec_units, batch_sz):
+    super(Decoder, self).__init__()
+    self.batch_sz = batch_sz
+    self.dec_units = dec_units
+    self.lstm = tf.keras.layers.LSTM(self.dec_units, return_sequences=True, return_state=True, recurrent_initializer='glorot_uniform')
+
+    #目前对只对客流进行预测，
+    self.fc = tf.keras.layers.Dense(result_size)
+
+    # 通过Attention对第1次编码输出加权
+    self.attention = Attention(self.dec_units)
+
+  def call(self,hidden, enc_output):
+    # 编码器输出 （enc_output） 的形状 == （批大小，隐藏层大小）
+    context_vector, attention_weights = self.attention(hidden, enc_output)
+
+    context_vector_ = tf.expand_dims(context_vector, 1)
+
+    output, state, _ = self.lstm(context_vector_)
+
+    # 输出的形状 == （批大小 * 1，隐藏层大小）
+    output = tf.reshape(output, (-1, output.shape[2]))
+
+    # 输出的形状 == （批大小，vocab）
+    x = self.fc(output)
+
+    return x
+
+@tf.function
+def train_step(input, targ, enc_hidden):
+
+    with tf.GradientTape() as tape:
+        enc_output, enc_hidden_h,  enc_hidden_c= encoder(input, enc_hidden)
+        enc_hidden = [enc_hidden_h,  enc_hidden_c]
+
+        predictions = decoder(enc_hidden, enc_output)
+
+        loss = tf.keras.losses.MSE(targ, predictions)
+
+    variables = encoder.trainable_variables + decoder.trainable_variables
+    gradients = tape.gradient(loss, variables)
+    optimizer.apply_gradients(zip(gradients, variables))
+
+    loss_ = tf.reduce_sum(loss)
+
+    return loss_
+
+def cal_pop_fitness(pop):
+
+    fitness =[]
+    for p in pop:
+
+        encoder = Encoder(enc_units=units_num, batch_sz=batch_szie)
+        init_hidden = encoder.initialize_hidden_state()
+
+        decoder = Decoder(result_size, units_num, batch_sz=batch_szie)
+
+        steps_per_epoch = input.shape[0] // batch_szie
+
+        for epoch in range(EPOCHS):
+
+            enc_hidden = [init_hidden, init_hidden]
+            total_loss = 0
+
+            for (batch, (inp, targ)) in enumerate(dataset_.take(steps_per_epoch)):
+                batch_loss = train_step(inp, targ, enc_hidden)
+                total_loss += batch_loss
+
+            fitness.append(-total_loss)
+
+    return np.array(fitness)
+
+def select_mating_pool(pop, fitness, num_parents):
+    # Selecting the best individuals in the current generation as parents for producing the offspring of the next generation.
+    parents = np.empty((num_parents, pop.shape[1]))
+    for parent_num in range(num_parents):
+        max_fitness_idx = np.where(fitness == np.max(fitness))
+        max_fitness_idx = max_fitness_idx[0][0]
+        parents[parent_num, :] = pop[max_fitness_idx, :]
+        fitness[max_fitness_idx] = -99999999999
+    return parents
+
+def crossover(parents, offspring_size):
+    offspring = np.empty(offspring_size)
+    # The point at which crossover takes place between two parents. Usually, it is at the center.
+    crossover_point = np.uint8(offspring_size[1]/2)
+
+    for k in range(offspring_size[0]):
+        # Index of the first parent to mate.
+        parent1_idx = k%parents.shape[0]
+        # Index of the second parent to mate.
+        parent2_idx = (k+1)%parents.shape[0]
+        # The new offspring will have its first half of its genes taken from the first parent.
+        offspring[k, 0:crossover_point] = parents[parent1_idx, 0:crossover_point]
+        # The new offspring will have its second half of its genes taken from the second parent.
+        offspring[k, crossover_point:] = parents[parent2_idx, crossover_point:]
+    return offspring
+
+def mutation(offspring_crossover, num_mutations=1):
+    mutations_counter = np.uint8(offspring_crossover.shape[1] / num_mutations)
+    # Mutation changes a number of genes as defined by the num_mutations argument. The changes are random.
+    for idx in range(offspring_crossover.shape[0]):
+        gene_idx = mutations_counter - 1
+        for mutation_num in range(num_mutations):
+            # The random value to be added to the gene.
+            random_value = np.random.uniform(-1.0, 1.0, 1)
+            offspring_crossover[idx, gene_idx] = offspring_crossover[idx, gene_idx] + random_value
+            gene_idx = gene_idx + mutations_counter
+    return offspring_crossover
+
+units_num = 32
+batch_szie = 16
+time_step = 16
+attention_units = 16
+step = 5
+BUFFER_SIZE = 10000
+result_size = 1
+
+EPOCHS = 1000
+
+X, Y = csv_to_dataset(r'Metro_Interstate_Traffic_Volume.csv')
+
+input = np.array([X[step_count*step: step_count*step+time_step] for step_count in range((X.shape[0]-time_step-1)//step)])
+targ = np.array([Y[step_count*step+time_step+1] for step_count in range((X.shape[0]-time_step-1)//step)])
+
+input_ = input[:int(input.shape[0]*0.8)]
+targ_ = targ[:int(targ.shape[0]*0.8)]
+
+test_input = input[int(input.shape[0]*0.8):]
+targ_input = targ[int(input.shape[0]*0.8):]
+
+
+dataset = tf.data.Dataset.from_tensor_slices((input_, targ_)).shuffle(BUFFER_SIZE)
+
+dataset_ = dataset.batch(batch_szie, drop_remainder=True)
+
+
+#遗传算法部分
+
+#设置用于初始化lstm模型的参数
+new_population = size=np.ones((6, 1))
+new_population[0, :] = [8]
+new_population[1, :] = [16]
+new_population[2, :] = [32]
+new_population[3, :] = [64]
+new_population[4, :] = [128]
+new_population[5, :] = [256]
+
+best_outputs = []
+num_generations = 1000
+
+for generation in range(num_generations):
+    print("Generation : ", generation)
+    # Measuring the fitness of each chromosome in the population.
+    fitness = cal_pop_fitness(new_population)
+    print("Fitness")
+    print(fitness)
+
+
+# example_input_batch, example_target_batch = next(iter(dataset_))
+#
+# encoder = Encoder(enc_units=units_num, batch_sz=batch_szie)
+#
+# optimizer = tf.keras.optimizers.Adam()
+#
+# init_hidden = encoder.initialize_hidden_state()
+#
+# decoder = Decoder(result_size, units_num, batch_sz=batch_szie)
+#
+# steps_per_epoch = input.shape[0] // batch_szie
+#
+# checkpoint_dir = './training_checkpoints'
+# checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+# checkpoint = tf.train.Checkpoint(optimizer=optimizer, encoder=encoder, decoder=decoder)
+#
+#
+#
+# for epoch in range(EPOCHS):
+#
+#     enc_hidden = [init_hidden, init_hidden]
+#     total_loss = 0
+#
+#     for (batch, (inp, targ)) in enumerate(dataset_.take(steps_per_epoch)):
+#
+#         batch_loss = train_step(inp, targ, enc_hidden)
+#         total_loss += batch_loss
+#
+#     print('Epoch {} Loss {:.4f}'.format(epoch + 1, total_loss/steps_per_epoch))
+#
+#     if epoch % 200 == 0:
+#         checkpoint.save(file_prefix=checkpoint_prefix)
 
 
 
-if __name__ == "__main__":
-    main()
+
+
+
