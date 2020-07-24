5f4717b09834ba9b692a50199940a0054d2ef007
cuifeng <cuifeng_fine@163.com>
2020-07-24 17:30:15 +0800

    读取csv

--- ga_lstm.py
+++ ga_lstm.py
@@ -1,128 +1,38 @@
 import tensorflow as tf
 import numpy as np
-
-import os
+from sklearn.metrics import mean_squared_error
 
 from data_process import csv_to_dataset
 
 
-tf.keras.backend.set_floatx('float32')
-
-class Attention(tf.keras.layers.Layer):
-  def __init__(self, units_num):
-    super(Attention, self).__init__()
-    self.W1 = tf.keras.layers.Dense(units_num)
-    self.W2 = tf.keras.layers.Dense(units_num)
-    self.W3 = tf.keras.layers.Dense(units_num)
-    self.V = tf.keras.layers.Dense(1)
-
-  def call(self, query, values):
-
-    hidden_with_time_axis_h = tf.expand_dims(query[0], 1)
-    hidden_with_time_axis_c = tf.expand_dims(query[1], 1)
-
-    score = self.V(tf.nn.tanh(self.W1(values) + self.W2(hidden_with_time_axis_h)+self.W3(hidden_with_time_axis_c)))
-
-    attention_weights = tf.nn.softmax(score, axis=1)
-
-    context_vector = attention_weights * values
-    context_vector = tf.reduce_sum(context_vector, axis=1)
-
-    return context_vector, attention_weights
-
-class Encoder(tf.keras.Model):
-    def __init__(self, enc_units, batch_sz):
-        super(Encoder, self).__init__()
-        self.batch_sz = batch_sz
-        self.enc_units = enc_units
-
-        #可以考虑通过ga调整的参数  recurrent_initializer 有多个值（orthogonal、glorot_uniform）
-        self.lstm = tf.keras.layers.LSTM(self.enc_units, return_sequences=True, return_state=True, recurrent_initializer='glorot_uniform')
-
-    def call(self, x, hidden):
-
-        self.lstm.states[0] = hidden[0]
-        self.lstm.states[1] = hidden[1]
-
-        output, state_h, state_c = self.lstm(x)
-
-        return output, state_h, state_c
-
-    def initialize_hidden_state(self):
-        return tf.zeros([self.batch_sz, self.enc_units])
-
-class Decoder(tf.keras.Model):
-  def __init__(self, result_size, dec_units, batch_sz):
-    super(Decoder, self).__init__()
-    self.batch_sz = batch_sz
-    self.dec_units = dec_units
-    self.lstm = tf.keras.layers.LSTM(self.dec_units, return_sequences=True, return_state=True, recurrent_initializer='glorot_uniform')
-
-    #目前对只对客流进行预测，
-    self.fc = tf.keras.layers.Dense(result_size)
-
-    # 通过Attention对第1次编码输出加权
-    self.attention = Attention(self.dec_units)
-
-  def call(self,hidden, enc_output):
-    # 编码器输出 （enc_output） 的形状 == （批大小，隐藏层大小）
-    context_vector, attention_weights = self.attention(hidden, enc_output)
-
-    context_vector_ = tf.expand_dims(context_vector, 1)
-
-    output, state, _ = self.lstm(context_vector_)
-
-    # 输出的形状 == （批大小 * 1，隐藏层大小）
-    output = tf.reshape(output, (-1, output.shape[2]))
-
-    # 输出的形状 == （批大小，vocab）
-    x = self.fc(output)
-
-    return x
-
-@tf.function
-def train_step(input, targ, enc_hidden):
-
-    with tf.GradientTape() as tape:
-        enc_output, enc_hidden_h,  enc_hidden_c= encoder(input, enc_hidden)
-        enc_hidden = [enc_hidden_h,  enc_hidden_c]
-
-        predictions = decoder(enc_hidden, enc_output)
-
-        loss = tf.keras.losses.MSE(targ, predictions)
-
-    variables = encoder.trainable_variables + decoder.trainable_variables
-    gradients = tape.gradient(loss, variables)
-    optimizer.apply_gradients(zip(gradients, variables))
-
-    loss_ = tf.reduce_sum(loss)
-
-    return loss_
-
 #遗传算法部分
+#调整模型参数
+#
 
-def cal_pop_fitness(pop):
+def cal_pop_fitness(population):
+    """
+
+    :param pop:
+    :return:
+    """
 
     fitness =[]
-    for p in pop:
+    for p in population:
+        batch_size, time_step, step, units = p
 
-        encoder = Encoder(enc_units=units_num, batch_sz=batch_szie)
-        init_hidden = encoder.initialize_hidden_state()
+        dataset, test_input, test_targ = get_data(batch_size=batch_size, time_step=time_step, step=step)
+        model = get_model(units=units, time_step=time_step)
+        model.summary()
 
-        decoder = Decoder(result_size, units_num, batch_sz=batch_szie)
+        model.fit(dataset, epochs=1)
 
-        steps_per_epoch = input.shape[0] // batch_szie
+        prec = model.predict(test_input)
 
-        for epoch in range(EPOCHS):
+        mse = mean_squared_error(test_targ, prec);
 
-            enc_hidden = [init_hidden, init_hidden]
-            total_loss = 0
+        fitness.append(mse)
 
-            for (batch, (inp, targ)) in enumerate(dataset_.take(steps_per_epoch)):
-                batch_loss = train_step(inp, targ, enc_hidden)
-                total_loss += batch_loss
-
-            fitness.append(-total_loss)
+        tf.keras.backend.clear_session()
 
     return np.array(fitness)
 
@@ -155,94 +65,79 @@ def crossover(parents, offspring_size):
 def mutation(offspring_crossover, num_mutations=1):
     mutations_counter = np.uint8(offspring_crossover.shape[1] / num_mutations)
     # Mutation changes a number of genes as defined by the num_mutations argument. The changes are random.
+    mutation_range = {0: 60, 1: 60, 2: 12, 3: 500}
+    mutation_min = {0: 8, 1: 8, 2: 8, 3: 64}
+    mutation_max = {0: 128, 1: 128, 2: 32, 3: 1024}
+
     for idx in range(offspring_crossover.shape[0]):
         gene_idx = mutations_counter - 1
         for mutation_num in range(num_mutations):
             # The random value to be added to the gene.
-            random_value = np.random.uniform(-1.0, 1.0, 1)
+            random_value = np.random.uniform(-1.0, 1.0, 1)*mutation_range[gene_idx]
+            random_value = np.clip(random_value, mutation_min[gene_idx], mutation_max[gene_idx])
             offspring_crossover[idx, gene_idx] = offspring_crossover[idx, gene_idx] + random_value
+
             gene_idx = gene_idx + mutations_counter
     return offspring_crossover
 
-units_num = 32
-batch_szie = 16
-time_step = 16
-attention_units = 16
-step = 5
 
+#lstm模型部分
+#
 
-BUFFER_SIZE = 10000
-result_size = 1
+def get_model(units=8, time_step=32):
+    """
+    :param units:
+    :param time_step:
+    :return:
+    """
 
-EPOCHS = 1000
+    input_ = tf.keras.layers.Input(shape=(time_step,10))
+    #使用1层lstm作为编码器
+    enc_output, enc_hidden_h, enc_hidden_c = tf.keras.layers.LSTM(units,
+                                                                  return_sequences=True,
+                                                                  return_state=True,
+                                                                  recurrent_initializer='glorot_uniform')(input_)
 
-X, Y = csv_to_dataset(r'Metro_Interstate_Traffic_Volume.csv')
+    W1 = tf.keras.layers.Dense(units)
+    W2 = tf.keras.layers.Dense(units)
+    W3 = tf.keras.layers.Dense(units)
+    V = tf.keras.layers.Dense(1)
 
-input = np.array([X[step_count*step: step_count*step+time_step] for step_count in range((X.shape[0]-time_step-1)//step)])
-targ = np.array([Y[step_count*step+time_step+1] for step_count in range((X.shape[0]-time_step-1)//step)])
+    hidden_with_time_axis_h = tf.expand_dims(enc_hidden_h, 1)
+    hidden_with_time_axis_c = tf.expand_dims(enc_hidden_c, 1)
 
-#mse0 = 0.01
-input_ = input[:int(input.shape[0]*0.8)]
-targ_ = targ[:int(targ.shape[0]*0.8)]
+    score = V(tf.nn.tanh(W1(enc_output) + W2(hidden_with_time_axis_h) + W3(hidden_with_time_axis_c)))
+    attention_weights = tf.nn.softmax(score, axis=1)
 
+    context_vector = attention_weights * enc_output
+    context_vector = tf.reduce_sum(context_vector, axis=1)
 
-#mse1 = 0.2
-test_input = input[int(input.shape[0]*0.8):]
-test_targ = targ[int(input.shape[0]*0.8):]
+    context_vector_ = tf.expand_dims(context_vector, 1)
 
+    dec_output, dec_hidden_h, dec_hidden_c = tf.keras.layers.LSTM(units,
+                                                                  return_sequences=True,
+                                                                  return_state=True,
+                                                                  recurrent_initializer='glorot_uniform')(context_vector_)
 
-dataset = tf.data.Dataset.from_tensor_slices((input_, targ_)).shuffle(BUFFER_SIZE)
+    result = tf.keras.layers.Dense(1)(dec_output)
+    result_ = tf.keras.layers.Reshape((1,))(result)
 
-dataset_ = dataset.batch(batch_szie, drop_remainder=True)
+    model = tf.keras.Model(input_, result_)
 
-example_input_batch, example_target_batch = next(iter(dataset_))
+    model.compile(optimizer='adam',
+                  loss='mse',
+                  metrics=['mse',])
 
-encoder = Encoder(enc_units=units_num, batch_sz=batch_szie)
-
-optimizer = tf.keras.optimizers.Adam()
-
-init_hidden = encoder.initialize_hidden_state()
-
-decoder = Decoder(result_size, units_num, batch_sz=batch_szie)
-
-steps_per_epoch = input.shape[0] // batch_szie
-
-checkpoint_dir = './training_checkpoints'
-checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
-checkpoint = tf.train.Checkpoint(optimizer=optimizer, encoder=encoder, decoder=decoder)
-
-
-
-for epoch in range(EPOCHS):
-
-    enc_hidden = [init_hidden, init_hidden]
-    total_loss = 0
-
-    for (batch, (inp, targ)) in enumerate(dataset_.take(steps_per_epoch)):
-
-        batch_loss = train_step(inp, targ, enc_hidden)
-        total_loss += batch_loss
-
-    print('Epoch {} Loss {:.4f}'.format(epoch + 1, total_loss/steps_per_epoch))
-
-    if epoch % 200 == 0:
-        checkpoint.save(file_prefix=checkpoint_prefix)
-
-
-
-def get_model(layers=2, units=8):
-
-    input_ = tf.keras.layers.Input(shape=())
-
-    for i in  range(layers):
-        enc
-
-
-    model = tf.keras.Model()
     return model
 
 
-def get_data(batch_size=32, time_step = 16):
+def get_data(batch_size=32, time_step = 32, step=5):
+    """
+    :param batch_size:
+    :param time_step:
+    :param step:
+    :return:
+    """
 
     X, Y = csv_to_dataset(r'Metro_Interstate_Traffic_Volume.csv')
 
@@ -253,12 +148,63 @@ def get_data(batch_size=32, time_step = 16):
     input_ = input[:int(input.shape[0]*0.8)]
     targ_ = targ[:int(targ.shape[0]*0.8)]
 
-    #测试集
+    #测试集,用于计算fitness
     test_input = input[int(input.shape[0]*0.8):]
     test_targ = targ[int(input.shape[0]*0.8):]
 
     dataset = tf.data.Dataset.from_tensor_slices((input_, targ_)).shuffle(BUFFER_SIZE)
 
-    dataset_ = dataset.batch(batch_szie, drop_remainder=True)
+    dataset_ = dataset.batch(batch_size, drop_remainder=True)
 
     return dataset_, test_input, test_targ
+
+
+def main():
+
+    #population =[[batch_size, time_step, step, units], ...]
+
+    batch_size = np.random.randint(low=8, high=128, size=(8,))
+    time_step = np.random.randint(low=8, high=128, size=(8,))
+    step = np.random.randint(low=8, high=32, size=(8,))
+    units = np.random.randint(low=64, high=1024, size=(8,))
+
+    population = np.array(list(zip(batch_size, time_step, step, units)), dtype=np.int)
+
+    num_generations = 1000
+
+    for generation in range(num_generations):
+        print("Generation {}".format(generation))
+
+        fitness = cal_pop_fitness(population)
+
+        parents = select_mating_pool(population, fitness, num_parents_mating)
+
+        offspring_crossover = crossover(parents,
+                                        offspring_size=(pop_size[0] - parents.shape[0], 4))
+
+        offspring_mutation = mutation(offspring_crossover, num_mutations=2)
+
+        population[0:parents.shape[0], :] = parents
+        population[parents.shape[0]:, :] = offspring_mutation
+
+        print(population)
+
+    fitness = cal_pop_fitness(population)
+    index = np.argmax(fitness)
+    best_model_parm = population[index]
+    batch_size, time_step, step, units = best_model_parm
+
+    model = get_model(units, time_step)
+    model.summary()
+
+
+
+
+if __name__ == "__main__":
+
+    BUFFER_SIZE = 10000
+    sol_per_pop = 8
+    num_parents_mating = 4
+    pop_size = (sol_per_pop, 4)
+
+    main()
